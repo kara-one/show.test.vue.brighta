508adb07466229f33cbdb966b270523a228a203a
Serhii Karavashkin <ksawl@narod.ru>
2020-09-05 22:47:15 +0300

    add: vuetify

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import App from './App.vue';
 import Vue from 'vue';
 import store from './store';
+import vuetify from './plugins/vuetify';
+import '@babel/polyfill';
 
 new Vue({
   store,
+  vuetify,
   render: h => h(App)
 }).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
-const path = require("path");
+const path = require('path');
 module.exports = {
   chainWebpack: config => {
-    const oneOfsMap = config.module.rule("scss").oneOfs.store;
+    const oneOfsMap = config.module.rule('scss').oneOfs.store;
     oneOfsMap.forEach(item => {
       item
-        .use("sass-resources-loader")
-        .loader("sass-resources-loader")
+        .use('sass-resources-loader')
+        .loader('sass-resources-loader')
         .options({
           resources: [
-            path.resolve(__dirname, "src/assets/styles/_vars.scss"),
-            path.resolve(__dirname, "src/assets/styles/_mixins.scss")
+            path.resolve(__dirname, 'src/assets/styles/_vars.scss'),
+            path.resolve(__dirname, 'src/assets/styles/_mixins.scss')
           ]
         })
         .end();
